f71d31d58ae2df7d7e192bdb36c2fe34f1c1943a
Luka Hietala <luka.hietala08@gmail.com>
2025-11-25 13:35:07 +0200

    asiallista

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,4 @@
 -- remaps
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 -- plugin spesific ones are in their confs 
+vim.keymap.set("n", "sex", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) 
